46962cbe30055e3a110765aa91b25d933e3ef177
minji105 <ajhjmj9@naver.com>
2024-03-26 01:32:15 +0900

    commit

--- goTest.js
+++ goTest.js
@@ -1,6 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-      window.location.href = './test.html';
-    }, 5000);
-  });
-  
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    window.location.href = './test.html';
+  }, 5000);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const loadingText = document.getElementById('loading-text');
+
+  if (loadingText) {
+    setTimeout(function () {
+      loadingText.style.display = 'none'; 
+    }, 3100);
+  }
+});
